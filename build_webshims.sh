180f6eb0b063e7f8e16bcb990fd8445c7082f0bb
paradust7 <102263465+paradust7@users.noreply.github.com>
2022-05-22 11:42:09 -0700

    Pin webshims revision

--- build_webshims.sh
+++ build_webshims.sh
@@ -1,11 +1,29 @@
 #!/bin/bash -eux
 
+WEBSHIMS_REV=01b29b520fa2c97f9c8f79b4bfd5ae67ecba6e06
+
 source common.sh
 
 if [ ! -d sources/webshims ]; then
   pushd sources
   git clone "https://github.com/paradust7/webshims.git" webshims
   popd
+fi
+
+pushd sources/webshims
+REV=`git rev-parse HEAD`
+popd
+
+if [ "$REV" != "$WEBSHIMS_REV" ]; then
+    set +x
+    echo "---------------------------------------------------------------"
+    echo "ERROR: sources/webshim on wrong revision"
+    echo "--------------------------------------------------------------"
+    echo "Expected revision: $WEBSHIMS_REV"
+    echo "Actual revision: $REV"
+    echo "---------------------------------------------------------------"
+    echo "Please 'git pull' or delete this directory before proceeding"
+    exit 1
 fi
 
 pushd "$BUILD_DIR"
