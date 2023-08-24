8a495e165d7c170838afead94153e26627dd0f28
paradust7 <102263465+paradust7@users.noreply.github.com>
2023-08-24 16:25:16 +0000

    Add multiplayer to sample index.html

--- build_sqlite3.sh
+++ build_sqlite3.sh
@@ -6,7 +6,7 @@ unpack_source SQLite
 
 pushd "$BUILD_DIR/SQLite"
 export BUILD_CC="gcc"
-emconfigure ./configure --disable-shared --prefix="$INSTALL_DIR" cross_compiling=yes
+emconfigure ./configure --disable-tcl --disable-shared --prefix="$INSTALL_DIR" cross_compiling=yes
 emmake make
 emmake make install
 
